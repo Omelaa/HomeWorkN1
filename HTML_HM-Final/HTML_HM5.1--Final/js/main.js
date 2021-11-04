5b935072ac64c4c7de68017f4b97d14975d8ba9a
Omelaa <Ostpa28@gmail.com>
2021-11-04 23:15:56 +0200

    Галерея и слайдер есть + адаптив

--- HTML_HM-Final/HTML_HM5.1--Final/js/main.js
+++ HTML_HM-Final/HTML_HM5.1--Final/js/main.js
@@ -1,9 +1,10 @@
 
-new Swiper('.sliders__container',{
-    slidesPerView:1,
-    loop: true,
-    spaceBetween:20,
 
+
+new Swiper('.sliders__container', {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 10,
 
 
     navigation: {
@@ -13,8 +14,41 @@ new Swiper('.sliders__container',{
 
     pagination: {
         el: ".swiper-pagination",
+        clickable: true,
     },
+
+    autoplay:{
+        delay:1000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true,
+    }
 });
+
+ new Swiper('.sliders-line', {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 30,
+
+     navigation: {
+         nextEl: '.sliders__button-next',
+         prevEl: '.sliders__button-prev',
+     },
+
+     breakpoints: {
+         1024: {
+         },
+         770: {
+             slidesPerView: 2.5,
+             spaceBetween: 10,
+         },
+         580: {
+             slidesPerView: 1.5,
+             spaceBetween: 10,
+         },
+         320: {
+         }
+     }
+ });
 
 
 $('#myModal').on('shown.bs.modal', function () {
@@ -22,9 +56,9 @@ $('#myModal').on('shown.bs.modal', function () {
 })
 
 const moreMenu = document.querySelector('.header-nav__more');
-if (moreMenu){
+if (moreMenu) {
     const menuItems = document.querySelector('.header-nav__items');
-    moreMenu.addEventListener("click", function (){
+    moreMenu.addEventListener("click", function () {
         document.body.classList.toggle("_lock");
         moreMenu.classList.toggle('_active');
         menuItems.classList.toggle('_active');
